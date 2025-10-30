a1dcf3467aabd65f653b9ed7a9cde768ffefd0fe
Noah Anderson <noahanderson0414@gmail.com>
2025-10-30 11:46:43 -0400

    Allow bypassing wasm screen by holding backspace and enter

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,10 @@ async fn main() {
         loop {
             clear_background(BLACK);
 
+            if is_key_down(KeyCode::Backspace) && is_key_down(KeyCode::Enter) {
+                break;
+            }
+
             draw_text("WORK IN PROGRESS", 16., 48., 64., WHITE);
 
             next_frame().await
